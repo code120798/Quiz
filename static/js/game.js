54c8e2139705ddaee59f6fe335354d9fe1ce716d
Shamsul Ali <shamsul.ali@hotmail.com>
2022-10-14 17:30:30 +0100

    typos, new questions, mobile styling

--- static/js/game.js
+++ static/js/game.js
@@ -1,5 +1,5 @@
 const question = document.querySelector('#question');
-const choices = Array.from (document.querySelectorAll('.choice-text'));
+const choices = Array.from(document.querySelectorAll('.choice-text'));
 const progressText = document.querySelector('#progressText');
 const scoreText = document.querySelector('#score');
 const progressBarFull = document.querySelector('#progressBarFull');
@@ -12,56 +12,62 @@ let availableQuestions = []
 
 let questions = [
     {
-        question: 'What is 2+2?',
+        question: 'What is 2 + 2?',
         choice1: '2',
         choice2: '4',
         choice3: '21',
         choice4: '17',
-        answer: 4,
+        answer: 2,
     },
     {
-        question: 'What is 3+3?',
-        choice1: '2',
-        choice2: '6',
-        choice3: '21',
-        choice4: '17',
-        answer: 6,
+        question:
+            "The tallest building in the world is located in which city?",
+        choice1: "Dubai",
+        choice2: "New York",
+        choice3: "Shanghai",
+        choice4: "None of the above",
+        answer: 1,
     },
     {
-        question: 'What is 4+4?',
-        choice1: '2',
-        choice2: '8',
-        choice3: '21',
-        choice4: '17',
-        answer: 8,
+        question: "What percent of American adults believe that chocolate milk comes from brown cows?",
+        choice1: "20%",
+        choice2: "18%",
+        choice3: "7%",
+        choice4: "33%",
+        answer: 3,
     },
     {
-        question: 'What is 5+5?',
-        choice1: '2',
-        choice2: '10',
-        choice3: '21',
-        choice4: '17',
-        answer: 10,
+        question: "Approximately what percent of U.S. power outages are caused by squirrels?",
+        choice1: "10-20%",
+        choice2: "5-10%",
+        choice3: "15-20%",
+        choice4: "30%-40%",
+        answer: 1,
     }
 ]
+
 const SCORE_POINTS = 100
 const MAX_QUESTIONS = 4
 
 startGame = () => {
     questionCounter = 0
     score = 0
-    availableQuestions = {...questions}
+    availableQuestions = [...questions]
     getNewQuestion()
 }
+
 getNewQuestion = () => {
     if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
         localStorage.setItem('mostRecentScore', score)
-        return window.location.assign('/end.html')
+
+        return window.location.assign('/endpage.html')
     }
+
     questionCounter++
-    progressText.innerText = 'Question ${questionCounter} of ${MAX_QUESTIONS}'
-    progressBarFull.style.width = '${(questionCounter/MAX_QUESTIONS) * 100}%'
-    const questionIndex = Math.floor(Math.random() * availableQuestions.length)
+    progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`
+    progressBarFull.style.width = `${(questionCounter/MAX_QUESTIONS) * 100}%`
+    
+    const questionsIndex = Math.floor(Math.random() * availableQuestions.length)
     currentQuestion = availableQuestions[questionsIndex]
     question.innerText = currentQuestion.question
 
@@ -69,29 +75,39 @@ getNewQuestion = () => {
         const number = choice.dataset['number']
         choice.innerText = currentQuestion['choice' + number]
     })
+
     availableQuestions.splice(questionsIndex, 1)
+
     acceptingAnswers = true
 }
 
 choices.forEach(choice => {
     choice.addEventListener('click', e => {
-        if(acceptingAnswers) return
+        if(!acceptingAnswers) return
+
         acceptingAnswers = false
-        const selectedchoice = e.target
-        const selectedAnswer = selectedchoice.dataset['number']
+        const selectedChoice = e.target
+        const selectedAnswer = selectedChoice.dataset['number']
+
         let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect'
-        if(classToApply === 'correct'){
+
+        if(classToApply === 'correct') {
             incrementScore(SCORE_POINTS)
         }
-        selectedchoice.parentElement.classList.add(classToApply)
+
+        selectedChoice.parentElement.classList.add(classToApply)
+
         setTimeout(() => {
-            selectedchoice.parentElement.classList.remove(classToApply)
+            selectedChoice.parentElement.classList.remove(classToApply)
             getNewQuestion()
+
         }, 1000)
     })
 })
+
 incrementScore = num => {
     score +=num
     scoreText.innerText = score
 }
+
 startGame()
